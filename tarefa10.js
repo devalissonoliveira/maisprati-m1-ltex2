9d382ee85db1e14ce1a2dea1bd480087b32d958d
Alisson Oliveira <devalissonoliveira@gmail.com>
2024-09-20 22:25:54 -0300

    Criado o arquivo tarefa10 com a resolução da mesma

---
+++ tarefa10.js
@@ -0,0 +1,22 @@
+// Encontre o maior divisor comum (MDC) de dois números
+// Escreva um programa que use um loop do while para encontrar o maior divisor comum (MDC) de dois números fornecidos (por exemplo, 56 e 98).
+
+// Dica: Utilize o algoritmo de Euclides, onde você repete o processo de substituir o maior número pela diferença dos dois números até que ambos sejam iguais.
+
+const prompt = require('prompt-sync')();
+
+let num1 = Number(prompt('Informe o primeiro valor: '));
+let num2 = Number(prompt('Informe o segundo valor: '));
+
+let a = num1;
+let b = num2;
+
+do{
+    let temp = b;
+    b = a % b;
+    a = temp
+} while (b !== 0)
+
+const MDC = a;
+
+console.log(`O maior divisor comum de ${num1} e ${num2} é ${MDC}`);
